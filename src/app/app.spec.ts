4b08b2ed3010f3a3338d073bf68f77d19d845047
floriangourinat <florian.gourinat@gmail.com>
2025-09-01 19:21:19 +0200

    Test commit

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecf-2025');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecf-temp');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ecf-2025');
+  protected readonly title = signal('ecf-temp');
 }
